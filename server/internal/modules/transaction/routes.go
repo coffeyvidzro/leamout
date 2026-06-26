d118eb2db9cf0ecea4d48282e84978d64bbf2c15
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-06-26 22:57:39 +0000

    Add transaction routes

---
+++ server/internal/modules/transaction/routes.go
@@ -0,0 +1,10 @@
+package transaction
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(router gin.IRouter, handler *Handler, authMiddleware gin.HandlerFunc) {
+	transactions := router.Group("/transactions")
+	transactions.Use(authMiddleware)
+
+	transactions.GET("", handler.List)
+}
