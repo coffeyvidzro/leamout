828a8f9b3312dc18923f84594d8ecec61644f493
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-06-26 22:57:28 +0000

    Add transaction HTTP handler

--- server/internal/modules/transaction/handler.go
+++ server/internal/modules/transaction/handler.go
@@ -1,1 +1,35 @@
 package transaction
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/cuffeyvidzro/leamout/internal/http/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(service *Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) List(c *gin.Context) {
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "100"))
+	offset, _ := strconv.Atoi(c.DefaultQuery("offset", "0"))
+
+	items, err := h.service.List(c.Request.Context(), ListParams{
+		UserID: middleware.GetUserID(c),
+		Type:   Type(c.Query("type")),
+		Limit:  limit,
+		Offset: offset,
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to fetch transactions"})
+		return
+	}
+
+	c.JSON(http.StatusOK, items)
+}
