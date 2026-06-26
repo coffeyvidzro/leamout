ed93924fc5f32c3c74f1f8b7aa8c00c108c20bf3
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-06-26 23:01:24 +0000

    Add payment routes

---
+++ server/internal/modules/payment/routes.go
@@ -0,0 +1,10 @@
+package payment
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(router gin.IRouter, handler *Handler, authMiddleware gin.HandlerFunc) {
+	payments := router.Group("/payments")
+	payments.Use(authMiddleware)
+
+	payments.GET("", handler.List)
+}
