007cac4e27fd220f69d4c3e0dac81153ce032a8c
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-06-27 02:34:51 +0000

    Add payment methods catalog handler

---
+++ server/internal/modules/paymentmethod/handler.go
@@ -0,0 +1,25 @@
+package paymentmethod
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(service *Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) List(c *gin.Context) {
+	items := h.service.List(ListParams{
+		Country:  c.Query("country"),
+		Currency: c.Query("currency"),
+		Method:   c.Query("method"),
+	})
+
+	c.JSON(http.StatusOK, items)
+}
