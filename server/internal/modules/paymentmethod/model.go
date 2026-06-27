c99241a8d3c60b65e9315ba5aa47a287d7a9b823
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-06-27 02:33:58 +0000

    Add payment methods catalog module

---
+++ server/internal/modules/paymentmethod/model.go
@@ -0,0 +1,29 @@
+package paymentmethod
+
+type CatalogResponse struct {
+	Countries []Country `json:"countries"`
+}
+
+type Country struct {
+	Code             string   `json:"code"`
+	Name             string   `json:"name"`
+	CallingCode      string   `json:"calling_code"`
+	DefaultCurrency  string   `json:"default_currency"`
+	SupportedMethods []Method `json:"supported_methods"`
+}
+
+type Method struct {
+	Type      string     `json:"type"`
+	Operators []Operator `json:"operators"`
+}
+
+type Operator struct {
+	Code        string `json:"code"`
+	DisplayName string `json:"display_name"`
+}
+
+type ListParams struct {
+	Country  string
+	Currency string
+	Method   string
+}
