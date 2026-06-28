b86752f0e8f0d7401d8957b9ae9ed5792aadd61f
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-06-28 21:44:07 +0000

    feat: delegate customer meter credit application to ledger

--- server/internal/modules/customermeter/apply.go
+++ server/internal/modules/customermeter/apply.go
@@ -3,10 +3,11 @@ package customermeter
 import (
 	"context"
 
+	"github.com/cuffeyvidzro/leamout/internal/modules/usagecredit"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 )
 
 func (r *Repository) ApplySubscriptionCredits(ctx context.Context, tx pgx.Tx, userID, subscriptionID, checkoutID uuid.UUID, fallbackCustomerID *uuid.UUID) error {
-	return r.RefreshCreditsForSubscription(ctx, tx, userID, subscriptionID, fallbackCustomerID)
+	return usagecredit.NewRepository(r.db).ApplySubscriptionCredits(ctx, tx, userID, subscriptionID, checkoutID, fallbackCustomerID)
 }
