aaf76f36e94da0ce071209fbaaffb8d69d2edbd8
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-06-28 21:43:08 +0000

    feat: satisfy checkout usage credit interface

--- server/internal/modules/customermeter/apply.go
+++ server/internal/modules/customermeter/apply.go
@@ -1,1 +1,12 @@
 package customermeter
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+)
+
+func (r *Repository) ApplySubscriptionCredits(ctx context.Context, tx pgx.Tx, userID, subscriptionID, checkoutID uuid.UUID, fallbackCustomerID *uuid.UUID) error {
+	return r.RefreshCreditsForSubscription(ctx, tx, userID, subscriptionID, fallbackCustomerID)
+}
