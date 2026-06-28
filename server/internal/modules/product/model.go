966921173402a369194513f7cddc7230d79a5faf
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-06-28 18:24:45 +0000

    feat: add product benefits model fields

--- server/internal/modules/product/model.go
+++ server/internal/modules/product/model.go
@@ -3,20 +3,22 @@ package product
 import (
 	"time"
 
+	"github.com/cuffeyvidzro/leamout/internal/modules/benefit"
 	"github.com/cuffeyvidzro/leamout/internal/modules/price"
 	"github.com/google/uuid"
 )
 
 type Product struct {
-	ID          uuid.UUID      `json:"id"`
-	UserID      uuid.UUID      `json:"user_id"`
-	Name        string         `json:"name"`
-	Description *string        `json:"description,omitempty"`
-	Active      bool           `json:"active"`
-	Metadata    map[string]any `json:"metadata"`
-	Prices      []price.Price  `json:"prices"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
+	ID          uuid.UUID         `json:"id"`
+	UserID      uuid.UUID         `json:"user_id"`
+	Name        string            `json:"name"`
+	Description *string           `json:"description,omitempty"`
+	Active      bool              `json:"active"`
+	Metadata    map[string]any    `json:"metadata"`
+	Prices      []price.Price     `json:"prices"`
+	Benefits    []benefit.Benefit `json:"benefits"`
+	CreatedAt   time.Time         `json:"created_at"`
+	UpdatedAt   time.Time         `json:"updated_at"`
 }
 
 type CreateRequest struct {
@@ -32,4 +34,8 @@ type UpdateRequest struct {
 	Description *string        `json:"description,omitempty" binding:"omitempty,max=1000"`
 	Active      *bool          `json:"active,omitempty"`
 	Metadata    map[string]any `json:"metadata,omitempty"`
+}
+
+type UpdateBenefitsRequest struct {
+	Benefits []uuid.UUID `json:"benefits" binding:"required"`
 }
